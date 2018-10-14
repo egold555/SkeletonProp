50ae44fb63535d7444f3286bf2bc5061b943f18d
Eric Golde <eric@golde.org>
2018-10-14 13:27:15 -0700

    Add files via upload

---
+++ RenardSkeleton/RenardSkeleton.ino
@@ -0,0 +1,57 @@
+
+#include <Servo.h>
+#include <Renard.h>
+
+
+/*
+   Yellow: Pin 9
+   Red: +5V
+   Brown: GND
+*/
+
+// Values on the servo that correspond to mouth positions.
+const int FULLOPEN = 150;
+const int FULLCLOSED = 125;
+
+const int SERVO_MOUTH_PIN = 9;
+const int SERVO_HEAD_PIN = 11;
+const int EYESPIN = 3;
+
+Servo servoMouth;
+Servo servoHead;
+Renard renard(Serial, 3);
+
+
+void setup() {
+  // Open serial communications
+  Serial.begin(57600);
+  pinMode(EYESPIN, OUTPUT);
+  servoMouth.attach(SERVO_MOUTH_PIN);
+  servoMouth.write(FULLCLOSED);
+  servoHead.attach(SERVO_HEAD_PIN);
+  servoHead.write(90);
+}
+
+void loop() {
+  // Read renard
+  while (!renard.processInput())
+    ;
+
+  //Do Mouth
+  int channelValue = renard.channelValue(1);
+  int pos = map(channelValue, 0, 255, FULLCLOSED, FULLOPEN);
+  servoMouth.write(pos);
+
+  //Do Head
+  int channelValueHead = renard.channelValue(3);
+  int headPos = 90;
+  if(channelValueHead != 0){
+    headPos = map(channelValueHead, 0, 255, 0, 180);
+  }
+  servoHead.write(headPos);
+
+  //Do Eyes
+  int eyesValue = renard.channelValue(2);
+  analogWrite(EYESPIN, eyesValue);
+}
+
---
+++ SkeletonLights/SkeletonLights.ino
@@ -0,0 +1,41 @@
+#include <Adafruit_WS2801.h>
+#include <Renard.h>
+
+#define DATAPIN 7  // yellow
+#define CLOCKPIN 6  // green
+#define LIGHTS 5
+
+Adafruit_WS2801 strip = Adafruit_WS2801(LIGHTS, DATAPIN, CLOCKPIN);
+Renard renard(Serial, LIGHTS * 3);
+
+// Create a 24 bit color value from R,G,B
+uint32_t Color(byte r, byte g, byte b)
+{
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
+}
+void setup() {
+  Serial.begin(57600);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  while (!renard.processInput())
+      ;
+
+  for (int light = 0; light < LIGHTS; ++light) {
+    int r = renard.channelValue(light * 3 + 1);
+    int g = renard.channelValue(light * 3 + 2);
+    int b = renard.channelValue(light * 3 + 3);
+    strip.setPixelColor(light, Color(r, g, b));
+  }
+  strip.show();
+}
+
+
