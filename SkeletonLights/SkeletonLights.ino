cfdcb426582fd682755b2ab11fe2a1221668e3f4
Eric Golde <eric@golde.org>
2020-01-11 21:46:41 -0800

    Mergeing in parrot code for the new duel controller

--- SkeletonLights/SkeletonLights.ino
+++ SkeletonLights/SkeletonLights.ino
@@ -1,12 +1,22 @@
 #include <Adafruit_WS2801.h>
 #include <Renard.h>
 
-#define DATAPIN 7  // yellow
-#define CLOCKPIN 6  // green
+//yellow
+#define DATAPIN 9
+
+//green
+#define CLOCKPIN 4
+
 #define LIGHTS 5
 
+//Yellow
+#define PARROT_MOUTH 5
+
+//Green
+#define PARROT_BODY 6
+
 Adafruit_WS2801 strip = Adafruit_WS2801(LIGHTS, DATAPIN, CLOCKPIN);
-Renard renard(Serial, LIGHTS * 3);
+Renard renard(Serial, (LIGHTS * 3) + 2);
 
 // Create a 24 bit color value from R,G,B
 uint32_t Color(byte r, byte g, byte b)
@@ -23,6 +33,10 @@ void setup() {
   Serial.begin(57600);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+
+  pinMode(PARROT_MOUTH, OUTPUT);
+  pinMode(PARROT_BODY, OUTPUT);
+  
 }
 
 void loop() {
@@ -35,7 +49,22 @@ void loop() {
     int b = renard.channelValue(light * 3 + 3);
     strip.setPixelColor(light, Color(r, g, b));
   }
+  
   strip.show();
+
+
+  //parrot
+  if(renard.channelValue((LIGHTS * 3) + 1) > 127){
+      digitalWrite(PARROT_MOUTH, HIGH);
+  }
+  else {
+    digitalWrite(PARROT_MOUTH, LOW);
+  }
+
+  if(renard.channelValue((LIGHTS * 3) + 2) > 127){
+      digitalWrite(PARROT_BODY, HIGH);
+  }
+  else {
+    digitalWrite(PARROT_BODY, LOW);
+  }
 }
-
-
