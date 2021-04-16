ea488fa3d217c2554527b4c18ca3b8fe3a26cfb5
Eric Golde <eric@golde.org>
2021-04-16 14:36:14 -0700

    Added guitar lights & drummer lights

--- SkeletonLights/SkeletonLights.ino
+++ SkeletonLights/SkeletonLights.ino
@@ -1,22 +1,37 @@
+//OLD BOOTLOADER
+
 #include <Adafruit_WS2801.h>
 #include <Renard.h>
 
-//yellow
-#define DATAPIN 9
+//data pin - yellow
+//clock pin - green
+#define LIGHTS_SINGER_DATAPIN 9
+#define LIGHTS_SINGER_CLOCKPIN 4
+#define LIGHTS_SINGER_COUNT 5
 
-//green
-#define CLOCKPIN 4
+//data pin - yellow
+//clock pin - green
+#define LIGHTS_GUITAR_DATAPIN 3
+#define LIGHTS_GUITAR_CLOCKPIN 2
+#define LIGHTS_GUITAR_COUNT 1
 
-#define LIGHTS 5
+//data pin - yellow
+//clock pin - green
+#define LIGHTS_DRUMMER_DATAPIN 7
+#define LIGHTS_DRUMMER_CLOCKPIN 8
+#define LIGHTS_DRUMMER_COUNT 1
 
-//Yellow
+//Mouth - Yellow
+//Body - Green
 #define PARROT_MOUTH 5
-
-//Green
 #define PARROT_BODY 6
+#define PARROT_CHANNELS 2
 
-Adafruit_WS2801 strip = Adafruit_WS2801(LIGHTS, DATAPIN, CLOCKPIN);
-Renard renard(Serial, (LIGHTS * 3) + 2);
+Adafruit_WS2801 strip_skeleton = Adafruit_WS2801(LIGHTS_SINGER_COUNT, LIGHTS_SINGER_DATAPIN, LIGHTS_SINGER_CLOCKPIN);
+Adafruit_WS2801 strip_guitar = Adafruit_WS2801(LIGHTS_GUITAR_COUNT + 1, LIGHTS_GUITAR_DATAPIN, LIGHTS_GUITAR_CLOCKPIN);
+Adafruit_WS2801 strip_drummer = Adafruit_WS2801(LIGHTS_DRUMMER_COUNT, LIGHTS_DRUMMER_DATAPIN, LIGHTS_DRUMMER_CLOCKPIN);
+
+Renard renard(Serial, (LIGHTS_SINGER_COUNT * 3) + PARROT_CHANNELS + (LIGHTS_GUITAR_COUNT * 3) + (LIGHTS_DRUMMER_COUNT * 3));
 
 // Create a 24 bit color value from R,G,B
 uint32_t Color(byte r, byte g, byte b)
@@ -29,42 +44,72 @@ uint32_t Color(byte r, byte g, byte b)
   c |= b;
   return c;
 }
-void setup() {
+void setup()
+{
   Serial.begin(57600);
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip_skeleton.begin();
+  strip_skeleton.show(); // Initialize all pixels to 'off'
 
   pinMode(PARROT_MOUTH, OUTPUT);
   pinMode(PARROT_BODY, OUTPUT);
-  
+
+  strip_guitar.begin();
+  strip_guitar.show(); // Initialize all pixels to 'off'
+
+  strip_drummer.begin();
+  strip_drummer.show(); // Initialize all pixels to 'off'
 }
 
-void loop() {
+void loop()
+{
   while (!renard.processInput())
-      ;
+    ;
 
-  for (int light = 0; light < LIGHTS; ++light) {
-    int r = renard.channelValue(light * 3 + 1);
-    int g = renard.channelValue(light * 3 + 2);
-    int b = renard.channelValue(light * 3 + 3);
-    strip.setPixelColor(light, Color(r, g, b));
+  int r = 0;
+  int g = 0;
+  int b = 0;
+  for (int light = 0; light < LIGHTS_SINGER_COUNT; ++light)
+  {
+    r = renard.channelValue(light * 3 + 1);
+    g = renard.channelValue(light * 3 + 2);
+    b = renard.channelValue(light * 3 + 3);
+    strip_skeleton.setPixelColor(light, Color(r, g, b));
   }
-  
-  strip.show();
-
 
   //parrot
-  if(renard.channelValue((LIGHTS * 3) + 1) > 127){
-      digitalWrite(PARROT_MOUTH, HIGH);
+  if (renard.channelValue((LIGHTS_SINGER_COUNT * 3) + 1) > 127)
+  {
+    digitalWrite(PARROT_MOUTH, HIGH);
   }
-  else {
+  else
+  {
     digitalWrite(PARROT_MOUTH, LOW);
   }
 
-  if(renard.channelValue((LIGHTS * 3) + 2) > 127){
-      digitalWrite(PARROT_BODY, HIGH);
+  if (renard.channelValue((LIGHTS_SINGER_COUNT * 3) + 2) > 127)
+  {
+    digitalWrite(PARROT_BODY, HIGH);
   }
-  else {
+  else
+  {
     digitalWrite(PARROT_BODY, LOW);
   }
+
+  const int offsetGuitar = PARROT_CHANNELS + (LIGHTS_SINGER_COUNT * 3);
+  r = renard.channelValue(1 + offsetGuitar);
+  g = renard.channelValue(2 + offsetGuitar);
+  b = renard.channelValue(3 + offsetGuitar);
+  strip_guitar.setPixelColor(0, Color(r, g, b));
+  strip_guitar.setPixelColor(1, Color(r, g, b));
+
+  const int offsetDrummer = offsetGuitar + (LIGHTS_GUITAR_COUNT * 3);
+
+  r = renard.channelValue(1 + offsetDrummer);
+  g = renard.channelValue(2 + offsetDrummer);
+  b = renard.channelValue(3 + offsetDrummer);
+  strip_drummer.setPixelColor(0, Color(r, g, b));
+
+  strip_skeleton.show();
+  strip_guitar.show();
+  strip_drummer.show();
 }
